d9537c098fd7095cafb063a1b6dc24787553c94a
Arvid Lindström <arvid-lindstrom@live.se>
2024-01-31 21:22:41 +0100

    adding more examples, compiling and console logging

--- app.ts
+++ app.ts
@@ -1,2 +1,46 @@
 const a: string = 'This is a string';
 console.log(a.split(''));
+
+// Type Annotations
+let lang: string = 'TypeScript!';
+function greet(lang: string): string {
+    return `Hello, ${lang}!`;
+}
+
+console.log(greet(lang));
+
+// Type Inference:
+let message = 'Hello, TypeScript!'; // TypeScript infers the type string
+
+// Interfaces are used for specifying the shape of objects
+// They can then be used to specify the type of variables, 
+// function parameters and return values
+interface Person {
+    name: string;
+    age: number;
+    email?: string; // Optional property
+    readonly securitynumber: number; // cannot be changed
+}
+
+// interfaces can extend other interfaces
+interface Programmer extends Person {
+	favouriteLanguage: string;
+	currentStack: Array<string>;
+}
+
+let John: Person = {
+	name: 'John',
+	age: 39,
+	securitynumber: 555,
+}
+
+let JohnTS: Programmer = {
+	name: 'John',
+	age: 39,
+	email: 'john@codeman.ts',
+	securitynumber: 808,
+	favouriteLanguage: 'TypeScript',
+	currentStack: ['TypeScript', 'React', 'Node', 'Express', 'PostgreSQL']
+}
+
+console.log(JohnTS.favouriteLanguage, JohnTS.currentStack);
